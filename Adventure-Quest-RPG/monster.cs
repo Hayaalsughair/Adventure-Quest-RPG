57ad6635864b3ebd30fbef080789791aae01da3a
HANAN ALHADE <23041786@student.ltuc.com>
2024-06-30 21:59:30 +0300

    create classes monster and player

---
+++ Adventure-Quest-RPG/monster.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adventure_Quest_RPG
+{
+    public abstract class Monster
+    {
+        public string Name { get; set; }
+        public int Health { get; set; }
+        public int AttackPower { get; set; }
+        public int Defense { get; set; }
+
+
+        public Monster(string name , int health , int attack_power , int defense)
+        {
+            Name = name;
+            Health = health;
+            AttackPower = attack_power;
+            Defense = defense;
+        }
+
+        //public abstract void Attack(Player player);
+    }
+
+}
