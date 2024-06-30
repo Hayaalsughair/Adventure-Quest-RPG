56429a14d77eefd4a4c9cd37197d5532818fb323
Hayaalsughair <111651652+Hayaalsughair@users.noreply.github.com>
2024-07-01 02:22:45 +0300

    Create:Monster Charater Doc: Add README file

---
+++ Adventure-Quest-RPG/SullivanMonster.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adventure_Quest_RPG
+{
+    public class SullivanMonster : Monster
+    {
+        public SullivanMonster() :base ("Sullivan",80,40,5)
+        {
+        }
+
+    }
+}
