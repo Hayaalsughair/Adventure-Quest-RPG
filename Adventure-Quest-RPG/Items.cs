591ba256d35948b804d547aff3d14b29ae5ed574
Hayaalsughair <111651652+Hayaalsughair@users.noreply.github.com>
2024-07-05 22:05:13 +0300

    Edit: Add the dicription in items class

--- Adventure-Quest-RPG/Items.cs
+++ Adventure-Quest-RPG/Items.cs
@@ -18,7 +18,10 @@ namespace Adventure_Quest_RPG
     public class Potion : Items
     {
         public int Medicine { get; set; }
-
+        public Potion()
+        {
+            Description = "A magical potion that heals the player when consumed.";
+        }
         public override void Use(Player player)
         {
             player.Health += Medicine;
@@ -28,6 +31,10 @@ namespace Adventure_Quest_RPG
     public class Sword : Items 
     { 
         public int MasterSword  { get; set; }
+        public Sword()
+        {
+            Description = "A powerful sword that increases the player's attack power.";
+        }
         public override void Use(Player player)
         {
             player.AttackPower += MasterSword;
@@ -36,6 +43,10 @@ namespace Adventure_Quest_RPG
     public class Helmet: Items
     {
         public int MilitaryHelmet { get; set; }
+        public Helmet()
+        {
+            Description = "A sturdy helmet that provides additional defense to the player.";
+        }
         public override void Use(Player player)
         {
             player.Defense += MilitaryHelmet;
@@ -44,6 +55,10 @@ namespace Adventure_Quest_RPG
     public class Weapon : Items
     { 
         public int Axe { get; set; }
+        public Weapon()
+        {
+            Description = "An Axe that enhances the player's attack power.";
+        }
         public override void Use(Player player)
         {
             player.AttackPower += Axe;
@@ -52,6 +67,10 @@ namespace Adventure_Quest_RPG
     public class Armor : Items
     { 
         public int Vist { get; set; }
+        public Armor()
+        {
+            Description = "Protective armor that increases the player's defense.";
+        }
         public override void Use(Player player)
         {
             player.Defense += Vist;
