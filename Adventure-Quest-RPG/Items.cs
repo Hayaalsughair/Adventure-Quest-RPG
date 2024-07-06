c7cab5e81dff321ce412f1e784120eb895db9ca7
HANAN ALHADE <23041786@student.ltuc.com>
2024-07-06 14:18:25 +0300

    fix use method and create EquipItem method edit  items class

--- Adventure-Quest-RPG/Items.cs
+++ Adventure-Quest-RPG/Items.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace Adventure_Quest_RPG
 {
@@ -17,63 +18,75 @@ namespace Adventure_Quest_RPG
 
     public class Potion : Items
     {
-        public int Medicine { get; set; }
-        public Potion()
+        public int Medicine{ get; private set; }
+
+        public Potion (string name, int medicine)
         {
+            Name = name;
             Description = "A magical potion that heals the player when consumed.";
+            Medicine = medicine;
         }
+
         public override void Use(Player player)
         {
-            player.Health += Medicine;
+            player.EquipItem(this);
         }
 
     }
     public class Sword : Items 
     { 
         public int MasterSword  { get; set; }
-        public Sword()
+        public Sword(string name , int masterSword)
         {
+            Name = name;
             Description = "A powerful sword that increases the player's attack power.";
+            MasterSword = masterSword;
         }
         public override void Use(Player player)
         {
-            player.AttackPower += MasterSword;
+            player.EquipItem(this);
         }
     }
     public class Helmet: Items
     {
         public int MilitaryHelmet { get; set; }
-        public Helmet()
+        public Helmet(string name , int militaryHelmet)
         {
+            Name = name;
             Description = "A sturdy helmet that provides additional defense to the player.";
+            MilitaryHelmet = militaryHelmet;
         }
         public override void Use(Player player)
         {
-            player.Defense += MilitaryHelmet;
+            player.EquipItem(this);
         }
     }
     public class Weapon : Items
     { 
         public int Axe { get; set; }
-        public Weapon()
+        public Weapon(string name , int axe)
         {
+            Name = name;
             Description = "An Axe that enhances the player's attack power.";
+            Axe = axe;
         }
         public override void Use(Player player)
         {
-            player.AttackPower += Axe;
+            player.EquipItem(this);
         }
     }
     public class Armor : Items
     { 
         public int Vist { get; set; }
-        public Armor()
+        public Armor(string name , int vist)
         {
+            Name = name;
             Description = "Protective armor that increases the player's defense.";
+            Vist = vist;
         }
         public override void Use(Player player)
         {
-            player.Defense += Vist;
+            player.EquipItem(this);
         }
     }
    
--- Adventure-Quest-RPG/Player.cs
+++ Adventure-Quest-RPG/Player.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,7 +13,7 @@ namespace Adventure_Quest_RPG
     public class Player:Character
     {
         public Inventory Inventory { get; set; }
-        //Random random = new Random();
+        
         public Player( string name  , int attackPower = 60) {
 
             Name = name;
@@ -25,7 +26,38 @@ namespace Adventure_Quest_RPG
         public void UseItem(Items item)
         {
             item.Use(this);
-            Inventory.Remove(item);
+        }
+        public void EquipItem(Items item)
+        {
+            if (item is Potion)
+            {
+                AttackPower += ((Potion)item).Medicine;
+                Console.WriteLine($"{item.Name} equipped. Attack power increased by {((Potion)item).Medicine}.");
+            }
+            else
+            if (item is Sword)
+            {
+                AttackPower += ((Sword)item).MasterSword;
+                Console.WriteLine($"{item.Name} equipped. Attack power increased by {((Sword)item).MasterSword}.");
+            }
+            else
+            if (item is Weapon)
+            {
+                AttackPower += ((Weapon)item).Axe;
+                Console.WriteLine($"{item.Name} equipped. Attack power increased by {((Weapon)item).Axe}.");
+            }
+            else
+            if (item is Armor)
+            {
+                AttackPower += ((Armor)item).Vist;
+                Console.WriteLine($"{item.Name} equipped. Attack power increased by {((Armor)item).Vist}.");
+            }
+            else
+            if (item is Helmet)
+            {
+                Defense += ((Helmet)item).MilitaryHelmet;
+                Console.WriteLine($"{item.Name} equipped. Defense increased by {((Helmet)item).MilitaryHelmet}.");
+            }
         }
     }
 }
